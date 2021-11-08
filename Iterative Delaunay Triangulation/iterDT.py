0e79a9539112853807ea0b4063e65d956a232ff0
donnellca <donnellca@wustl.edu>
2021-11-08 03:11:19 -0600

    prog to iter

---
+++ Iterative Delaunay Triangulation/iterDT.py
@@ -0,0 +1,4 @@
+# Iterative Delaunay Triangulation
+# Algorithm for calculating new Delaunay Triangulations efficiently with points added over time
+# Algoirthm courtesy of Catie Jo at https://medium.com/@catiejo/2d-delaunay-triangulation-by-hand-without-a-voronoi-diagram-513156fd549f
+
